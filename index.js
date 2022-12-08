0de9d6154281b47a5de18b371dc2009e3aa62972
Gerjan Wielink <44892924+GerjanWielink@users.noreply.github.com>
2022-12-08 09:27:08 +0100

    extract data and snap methods

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-/**
- * Method which implements the 'snapping' logic for the starting point of the (new) appointment.
- *
- * @param {number} calendar_height Height, in pixels, of the calendar.
- * @param {number} pixel_value Raw selected pixel value. This value represent the earliest selected time (i.e. if one
- * drags from bottom to top, the dragged value will be passed).
- * @param {string} customer_id The value filled in the input field Customer.
- * @param {string} employee_id The value filled in the input field Employee.
- *
- * @return {number} Pixel value that needs to be used as the starting point of the (new) appointment.
- */
-function snapStart(calendar_height, pixel_value, customer_id, employee_id) {
-    return pixel_value;
-}
-
-/**
- * Method which implements the 'snapping' logic for the stopping point of the (new) appointment.
- *
- * @param {number} calendar_height Height, in pixels, of the calendar.
- * @param {number} pixel_value Raw selected pixel value. This value represent the latest selected time (i.e. if one
- * drags from bottom to top, the initial value will be passed).
- * @param {string} customer_id The value filled in the input field Customer.
- * @param {string} employee_id The value filled in the input field Employee.
- *
- * @return {number} Pixel value that needs to be used as the stopping point of the (new) appointment.
- */
-function snapStop(calendar_height, pixel_value, customer_id, employee_id) {
-    return pixel_value;
-}
-
-
 (function() {
     const CALENDAR_HEIGHT = 1440;
 
